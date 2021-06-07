c2c395162e770e9bcf68cc3ce58571133cd82cb3
suryaravikumar-pixer <suryaravikumar@gmail.com>
2021-06-07 16:57:20 +0530

    CURD functionality created on browsable api using restframework

---
+++ blogapi/posts/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=40)
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
+"""Update our database by first creating a new migration file and then running
+migrate to sync the database with our model changes"""
